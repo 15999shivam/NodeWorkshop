6f5676b68af62ee7e6cca24a46c04fa51f6a696e
Shivam <shivamgarg260@gmail.com>
2019-10-09 14:12:14 +0530

    added  class example with function and class keyword

--- jsbasics/objects.js
+++ jsbasics/objects.js
@@ -23,6 +23,49 @@ const me2 = {
 console.log(me2.name);
 me2.greet();
 
+const json = JSON.stringify(me);
+console.log(me);
+console.log(json); //do not stringyfy functions (can be done but not recomemded)
+
+const object = JSON.parse(json);
+console.log(object);
+
+// JSON has no means to represent a function. It is a data format designed for simplicity and compatibility across languages (and a function is the last thing that will be cross-language compatible).
+
+//=======================================================================================================================
+
+//classes using class keyword
+class Id {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayhi() {
+    console.log(`Hello ${this.name}, your age is ${this.age}`);
+  }
+}
+
+const shivam = new Id("Shivam", 20);
+console.log(shivam.name);
+shivam.sayhi();
+
+//==================================================================================================
+
+//class using function
+const Id2 = function(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayhi = function() {
+    console.log(`Hello ${this.name}, your age is ${this.age}`);
+  };
+};
+
+const aniket = new Id("aniket", 20);
+console.log(aniket.name);
+aniket.sayhi();
+
+//===============================================================================================================
+
 //array of objects
 
 // const products = [
